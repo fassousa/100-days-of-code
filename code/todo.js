763d73c23e85c052a61de7e7347a0ddad0d329b8
Fagnner Sousa <fagnnersousa@yahoo.com.br>
2018-09-18 23:33:46 -0300

    Create todoList object

--- code/todo.js
+++ code/todo.js
@@ -1,20 +1,3 @@
-let todos = ['item 1', 'item 2', 'item 3']
-
-function displayTodos() {
-  console.log('My todos:', todos);
-}
-
-function addTodos(todo) {
-  todos.push(todo);
-  displayTodos();
-}
-
-function changeTodo(position, newValue) {
-  todos[position] = newValue;
-  displayTodos();
-}
-
-function deleteTodo(position) {
-  todos.splice(position, 1);
-  displayTodos();
-}
+var todoList = {
+  todos: ['item 1', 'item 2', 'item 3']
+};
