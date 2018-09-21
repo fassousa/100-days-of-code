188be3e251928f25f5916449365a213b15bdd760
Fagnner Sousa <fagnnersousa@yahoo.com.br>
2018-09-20 23:54:20 -0300

    Create toggleAll function: if everything's true, make everything false

--- code/todo.js
+++ code/todo.js
@@ -33,5 +33,23 @@ var todoList = {
     var todo = this.todos[position];
     todo.completed = !todo.completed;
     this.displayTodos();
+  },
+  toggleAll: function() {
+    var totalTodos = this.todos.length;
+    var completedTodos = 0;
+    // Get number of completed todos.
+    for (var i = 0; i < totalTodos; i++) {
+      if (this.todos[i].completed === true) {
+        completedTodos++;
+      }
+    }
+    // Case 1: If everything's true, make everything false.
+    if (completedTodos === totalTodos) {
+      // Make everything false.
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = false;
+      }
+    }
+    this.displayTodos();
   }
 };
