f0068001d8fb399199c462b125bb6a54f403fb5c
Fagnner Sousa <fagnnersousa@yahoo.com.br>
2018-09-22 23:13:19 -0300

    Clicking 'Display todos' should run todoList.displayTodos

--- code/todo.js
+++ code/todo.js
@@ -58,3 +58,10 @@ var todoList = {
     this.displayTodos();
   }
 };
+
+var displayTodosButton = document.getElementById('displayTodosButton');
+console.log(displayTodosButton);
+
+displayTodosButton.addEventListener('click', function() {
+  todoList.displayTodos();
+});
