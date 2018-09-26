22968dfe841e4efb1befbd616696d1e90f935730
Fagnner Sousa <fagnnersousa@yahoo.com.br>
2018-09-25 23:36:49 -0300

    Clicking delete update todoList.todos and the DOM

--- code/todo.js
+++ code/todo.js
@@ -55,10 +55,8 @@ var handlers = {
     changeTodoTextInput.value = '';
     view.displayTodos();
   },
-  deleteTodo: function() {
-    var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
-    todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
-    deleteTodoPositionInput.value = '';
+  deleteTodo: function(position) {
+    todoList.deleteTodo(position);
     view.displayTodos();
   },
   toggleCompleted: function() {
@@ -99,10 +97,18 @@ var view = {
     deleteButton.textContent = "Delete";
     deleteButton.className = 'deleteButton';
     return deleteButton;
+  },
+  setUpEventListeners: function() {
+    var todosUl = document.querySelector('ul');
+
+    todosUl.addEventListener('click', function(event) {
+      var elementClicked = event.target;
+
+      if (elementClicked.className === 'deleteButton') {
+        handlers.deleteTodo(parseInt(elementClicked.parentNode.id));
+      }
+    });
   }
 };
 
-var todosUl = document.querySelector('ul');
-todosUl.addEventListener('click', function(event) {
-  console.log(event.target.parentNode.id);
-});
+view.setUpEventListeners();
