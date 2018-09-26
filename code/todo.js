e41a0aa33451f5edc48e2d394c19bf38b1d6da70
Fagnner Sousa <fagnnersousa@yahoo.com.br>
2018-09-25 23:25:20 -0300

    Delete button have access to the todo id

--- code/todo.js
+++ code/todo.js
@@ -101,3 +101,8 @@ var view = {
     return deleteButton;
   }
 };
+
+var todosUl = document.querySelector('ul');
+todosUl.addEventListener('click', function(event) {
+  console.log(event.target.parentNode.id);
+});
