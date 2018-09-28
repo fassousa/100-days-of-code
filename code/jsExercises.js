00a192c210c83567ae07657f7c2824c0bf825c64
Fagnner Sousa <fagnnersousa@yahoo.com.br>
2018-09-27 22:45:07 -0300

    1st January Program

--- code/jsExercises.js
+++ code/jsExercises.js
@@ -45,6 +45,15 @@ console.log(x);
 
 
 // 7. Write a JavaScript program to find 1st January is being a Sunday between 2014 and 2050.
+console.log('--------------------');
+for (var year = 2014; year <= 2050; year++)
+    {
+    var d = new Date(year, 0, 1);
+    if ( d.getDay() === 0 )
+        console.log("1st January is being a Sunday  "+year);
+    }
+console.log('--------------------');
+
 
 // 8. Write a JavaScript program where the program takes a random integer between 1 to 10,
 // the user is then prompted to input a guess number. If the user input matches with guess
