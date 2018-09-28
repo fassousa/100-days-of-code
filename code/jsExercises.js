5df9ca19e628582468c3e37602130161718cef30
Fagnner Sousa <fagnnersousa@yahoo.com.br>
2018-09-27 22:42:02 -0300

    Current date program

--- code/jsExercises.js
+++ code/jsExercises.js
@@ -7,6 +7,28 @@
 // 3. Write a JavaScript program to get the current date.  Go to the editor
 // Expected Output :
 // mm-dd-yyyy, mm/dd/yyyy or dd-mm-yyyy, dd/mm/yyyy
+var today = new Date();
+var dd = today.getDate();
+
+var mm = today.getMonth()+1;
+var yyyy = today.getFullYear();
+if(dd<10)
+{
+    dd='0'+dd;
+}
+
+if(mm<10)
+{
+    mm='0'+mm;
+}
+today = mm+'-'+dd+'-'+yyyy;
+console.log(today);
+today = mm+'/'+dd+'/'+yyyy;
+console.log(today);
+today = dd+'-'+mm+'-'+yyyy;
+console.log(today);
+today = dd+'/'+mm+'/'+yyyy;
+console.log(today);
 
 // 4. Write a JavaScript program to find the area of a triangle where lengths
 // of the three of its sides are 5, 6, 7.
